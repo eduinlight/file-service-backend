cc117acff738de61d25bb95b65584bf29368bc6f
Eduin Garcia Cordero <eduinlight@gmail.com>
2020-04-04 15:31:05 -0400

    -m

--- src/file_service_backend/schemas.clj
+++ src/file_service_backend/schemas.clj
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (ns file-service-backend.schemas
   (:require [schema.core :as s]))
 
@@ -36,42 +35,3 @@
   :path String,
   :name String,
 })
-=======
-(ns file-service-backend.schemas
-  (:require [schema.core :as s]))
-
-; (s/defschema Pizza
-;   {:name s/Str
-;     (s/optional-key :description) s/Str
-;     :size (s/enum :L :M :S)
-;     :origin {:country (s/enum :FI :PO)
-;             :city s/Str}})
-
-(s/defschema SFile {
-  :name String,
-  :path String,
-  :isFile Boolean,
-  :isDirectory Boolean,
-  :execute Boolean,
-  :read Boolean,
-  :write Boolean,
-})
-
-(s/defschema SChnageFilename {
-  :file_name String,
-  :new_name String,
-})
-
-(s/defschema SDirectorio {
-  :dir String,
-})
-
-(s/defschema SArchivo {
-  :file String,
-})
-
-(s/defschema SPath {
-  :path String,
-  :name String,
-})
->>>>>>> 771798138ecbec37c6e729313555ffad9f72264f
--- src/file_service_backend/utils.clj
+++ src/file_service_backend/utils.clj
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (ns file-service-backend.utils
   (:require [clojure.java.io :as io]))
 
@@ -11,18 +10,4 @@
     :execute (.canExecute file),
     :read (.canRead file),
     :write (.canWrite file),
-=======
-(ns file-service-backend.utils
-  (:require [clojure.java.io :as io]))
-
-(defn getInfo [file]
-  (-> {
-    :name (.getName file),
-    :path (.getCanonicalPath file),
-    :isFile (.isFile file),
-    :isDirectory (.isDirectory file),
-    :execute (.canExecute file),
-    :read (.canRead file),
-    :write (.canWrite file),
->>>>>>> 771798138ecbec37c6e729313555ffad9f72264f
   }))
